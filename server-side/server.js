5f9f1f79bf90f6a7df0e33a64019daee23aef254
apollo <lekhakdeepak10@gmail.com>
2024-01-07 00:32:41 -0500

    added home.jsx and style

--- server-side/server.js
+++ server-side/server.js
@@ -1,9 +1,45 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/demo');
+  console.log('db connected')
+}
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+
 const server = express();
 
-server.get('/demo', (req, res) => {
-    res.send('Hello, world')});
+server.use(cors());
+server.use(bodyParser.json());
 
-server.listen(8000, () =>{
-    console.log('listening on 8000')
+// CRUD - Create
+server.post('/demo',async (req,res)=>{
+    console.log(req.body);
+    let user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    const doc = await user.save();
+
+    console.log(doc);
+    res.json(doc);
+})
+
+server.get('/demo',async (req,res)=>{
+    const docs = await User.find({});
+    res.json(docs)
+})
+
+server.listen(8080,()=>{
+    console.log('server started')
 })
